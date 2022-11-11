db33cebefa4313b4f4dbf552bcb0ad2ba2a9c2ef
jooyong-boo <qnwndyd159@naver.com>
2022-11-11 22:32:41 +0900

    feat: redux 초기세팅

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 import AppDev from './AppDev';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 root.render(
 	<React.StrictMode>
-		<AppDev />
+		<Provider store={store}>
+			<AppDev />
+		</Provider>
 	</React.StrictMode>,
 );
