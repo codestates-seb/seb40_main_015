fd19687fc40fb2b44bb7879967a5caebce8f1bb6
SJUANS <lovejuan95@hanmail.net>
2022-11-12 14:21:16 +0900

    Feat : 백엔드 초기세팅

---
+++ backend/dongnebook/src/test/java/com/dongnebook/DongnebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongnebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongnebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
