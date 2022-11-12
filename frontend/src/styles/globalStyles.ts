1623e5f9afec86ea3ea17dcada68f1c84ac50e95
jooyong-boo <qnwndyd159@naver.com>
2022-11-12 12:08:06 +0900

    feat: styled-components 전역설정

--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
+import gangwon from '../fonts/gangwon.woff';
+import kotra from '../fonts/kotra.ttf';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+        font-family: 'gangwon';
+        src: local('gangwon'), local('gangwon');
+        font-style: normal;
+        src: url(${gangwon}) format('woff');
+  }
+  @font-face {
+        font-family: 'kotra';
+        src: local('kotra'), local('kotra');
+        font-style: normal;
+        src: url(${kotra}) format('truetype');
+  }
+
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -17,9 +32,14 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     border: 0;
-    font-size: 100%;
+    font-size: 10px;
     font: inherit;
     vertical-align: baseline;
+    font-family: 'gangwon', 'kotra';
+    background-color: #FBFBFB;
+    overflow-y: auto;
+    /* overflow: overlay; */
+    overflow-x: hidden;
   }
   /* HTML5 display-role reset for older browsers */
   article, aside, details, figcaption, figure, 
@@ -44,6 +64,16 @@ const GlobalStyle = createGlobalStyle`
     border-collapse: collapse;
     border-spacing: 0;
   }
+
+  body::-webkit-scrollbar {
+    width: 8px;
+    background: transparent;
+}
+
+body::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 100px;
+}
 `;
 
 export default GlobalStyle;
