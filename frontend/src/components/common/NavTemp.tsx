96974a4c7077fc2527c4f40413ff3f262f1bfa86
jooyong-boo <qnwndyd159@naver.com>
2022-11-13 23:38:56 +0900

    Feat: icon, Link 태그 위치 및 css width 변경

--- frontend/src/components/common/NavTemp.tsx
+++ frontend/src/components/common/NavTemp.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { TbBook } from 'react-icons/tb';
-import { BsChatDots } from 'react-icons/bs';
-import { FiUser } from 'react-icons/fi';
+import {
+	HiOutlineSearch,
+	HiOutlineBookOpen,
+	HiOutlineChat,
+	HiOutlineUser,
+	HiHome,
+} from 'react-icons/hi';
 import home from '../../assets/image/home.png';
 import { Link } from 'react-router-dom';
 
@@ -19,14 +22,14 @@ const NavTemp = () => {
 	const [menus, setMenus] = useState<MenuProps[]>([
 		{
 			id: 0,
-			icon: <AiOutlineSearch size="30" />,
+			icon: <HiOutlineSearch size="30" />,
 			text: '검색',
 			selected: false,
 			path: '/books/search',
 		},
 		{
 			id: 1,
-			icon: <TbBook size="30" />,
+			icon: <HiOutlineBookOpen size="30" />,
 			text: '대여목록',
 			selected: false,
 			path: '/history',
@@ -40,14 +43,14 @@ const NavTemp = () => {
 		},
 		{
 			id: 3,
-			icon: <BsChatDots size="30" />,
+			icon: <HiOutlineChat size="30" />,
 			text: '채팅',
 			selected: false,
 			path: '/chats',
 		},
 		{
 			id: 4,
-			icon: <FiUser size="30" />,
+			icon: <HiOutlineUser size="30" />,
 			text: '마이페이지',
 			selected: false,
 			path: '/profile',
@@ -68,13 +71,12 @@ const NavTemp = () => {
 			{menus.map(menu => {
 				const { id, icon, text, selected, path } = menu;
 				return (
-					<Box
-						key={id}
-						selected={selected}
-						onClick={() => handleChangeMenu(id)}>
-						{icon}
-						<Link to={path}>{text}</Link>
-					</Box>
+					<Link to={path} key={id}>
+						<Box selected={selected} onClick={() => handleChangeMenu(id)}>
+							{icon}
+							{text}
+						</Box>
+					</Link>
 				);
 			})}
 		</Container>
@@ -85,8 +87,8 @@ export default NavTemp;
 
 const Container = styled.div`
 	position: fixed;
-	/* width: 100vw; */
-	min-width: 425px;
+	width: 100%;
+	/* max-width: 425px; */
 	display: flex;
 	justify-content: space-around;
 	align-items: center;
@@ -101,5 +103,8 @@ interface BoxProps {
 }
 
 const Box = styled.div<BoxProps>`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 	color: ${props => (props.selected ? '#26795D' : '#000000')};
 `;
