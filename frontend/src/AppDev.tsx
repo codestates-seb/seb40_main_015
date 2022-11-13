0dc395fd26a81ec006e363ea12cd5483f6eb0c6f
jooyong-boo <93763666+jooyong-boo@users.noreply.github.com>
2022-11-13 12:08:26 +0900

    [FE] feat : 상인정보 페이지 및 하단바 구현 (#3)
    * feat: redux 초기세팅
    
    * feat: redux 초기세팅
    
    * feat: 메인로고 및 테스트용 이미지 추가
    
    * feat: 레이아웃 테스트
    
    * feat: 상인정보 책 리스트 및 하단바 구현
    
    * feat: 폰트 추가
    
    * feat: styled-components 테마 및 전역설정
    
    * feat: 기본 레이아웃 구성
    
    * feat: 상인정보 페이지
    
    * feat: styled-components 전역설정
    
    * feat: styled-components 테마설정

--- frontend/src/AppDev.tsx
+++ frontend/src/AppDev.tsx
@@ -1,12 +1,20 @@
+import styled from 'styled-components';
+import NavBar from './components/common/NavBar';
+import Layout from './pages/Layout';
+import LenderInfoPage from './pages/LenderInfoPage';
 import GlobalStyle from './styles/globalStyles';
 
 const AppDev = () => {
 	return (
-		<>
+		<Container>
 			<GlobalStyle />
-			<div>시작</div>
-		</>
+			<Layout />
+		</Container>
 	);
 };
 
 export default AppDev;
+
+const Container = styled.div`
+	width: 100vw;
+`;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 import AppDev from './AppDev';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import GlobalStyle from './styles/globalStyles';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/styles/theme';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 root.render(
 	<React.StrictMode>
-		<AppDev />
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<AppDev />
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>,
 );
