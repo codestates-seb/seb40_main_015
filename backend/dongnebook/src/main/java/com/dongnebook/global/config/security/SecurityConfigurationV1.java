e4ae093bc8ca736642737353c230a9a44b0bfdea
SJUANS <lovejuan95@hanmail.net>
2022-11-13 21:55:40 +0900

    Refactor : SecurityConfigurationV1 - @Bean 어노테이션 추가, 오타 수정

--- backend/dongnebook/src/main/java/com/dongnebook/global/config/security/SecurityConfigurationV1.java
+++ backend/dongnebook/src/main/java/com/dongnebook/global/config/security/SecurityConfigurationV1.java
@@ -1,11 +1,14 @@
 package com.dongnebook.global.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfigurationV1 {
-	public SecurutyFilterChain filterChain(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.headers().frameOptions().sameOrigin()
 			.and()
