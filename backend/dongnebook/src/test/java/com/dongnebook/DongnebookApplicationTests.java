90dc208a5be32900cee7433ba9e30d8f85b84fdf
SJUANS <lovejuan95@hanmail.net>
2022-11-14 01:15:15 +0900

    Bug: test source 내 test 파일들 모두 주석처리(사유: 빌드 오류)

--- backend/dongnebook/src/test/java/com/dongnebook/DongnebookApplicationTests.java
+++ backend/dongnebook/src/test/java/com/dongnebook/DongnebookApplicationTests.java
@@ -1,11 +1,11 @@
 package java.com.dongnebook;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class DongnebookApplicationTests {
-	@Test
-	void contextLoads() {
-	}
-}
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// public class DongnebookApplicationTests {
+// 	@Test
+// 	void contextLoads() {
+// 	}
+// }
--- backend/dongnebook/src/test/java/com/dongnebook/domain/helloSample/ui/HelloControllerTest.java
+++ backend/dongnebook/src/test/java/com/dongnebook/domain/helloSample/ui/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package java.com.dongnebook.domain.helloSample.ui;
-
-public class HelloControllerTest {
-}
+// package java.com.dongnebook.domain.helloSample.ui;
+//
+// public class HelloControllerTest {
+// }
