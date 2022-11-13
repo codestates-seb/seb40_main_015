01315830daab996c6916cf56b24d3325a5f18713
leeseongjun <thwn40@naver.com>
2022-11-13 17:34:28 +0900

    Feat : 공통 예외 처리 핸들러 추가

---
+++ backend/dongnebook/src/main/java/com/dongnebook/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.dongnebook.global.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.dongnebook.global.error.exception.BusinessException;
+import com.dongnebook.global.error.exception.ErrorCode;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		log.error("handleMethodArgumentNotValidException", e);
+		final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+		return ResponseEntity.badRequest().body(response);
+	}
+
+	@ExceptionHandler(BusinessException.class)
+	protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
+		log.error("handleEntityNotFoundException", e);
+		final ErrorCode errorCode = e.getErrorCode();
+		final ErrorResponse response = ErrorResponse.of(errorCode);
+		return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	protected ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
+		log.error("handleAccessDeniedException", e);
+		final ErrorResponse response = ErrorResponse.of(ErrorCode.HANDLE_ACCESS_DENIED);
+		return new ResponseEntity<>(response, HttpStatus.valueOf(ErrorCode.HANDLE_ACCESS_DENIED.getStatus()));
+	}
+
+	// @ExceptionHandler(JwtException.class)
+	// protected ResponseEntity<ErrorResponse> handleAccessDeniedException(JwtException e) {
+	// 	log.error("handleAccessDeniedException", e);
+	// 	final ErrorResponse response = ErrorResponse.of(ErrorCode.HANDLE_ACCESS_DENIED);
+	// 	return new ResponseEntity<>(response, HttpStatus.valueOf(ErrorCode.HANDLE_ACCESS_DENIED.getStatus()));
+	// }
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+		log.error("handleHttpRequestMethodNotSupportedException", e);
+		final ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
+		return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+	}
+
+
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+		log.error("handleEntityNotFoundException", e);
+		final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+
+		return ResponseEntity.internalServerError().body(response);
+	}
+
+
+}
