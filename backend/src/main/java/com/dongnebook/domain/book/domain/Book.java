2558cf69bde47399f5dd49a2aad54f64cd373c27
leeseongjun <thwn40@naver.com>
2022-11-15 00:52:30 +0900

    Refactor : FetchType.Lazy 추가

--- backend/src/main/java/com/dongnebook/domain/book/domain/Book.java
+++ backend/src/main/java/com/dongnebook/domain/book/domain/Book.java
@@ -11,6 +11,7 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -67,7 +68,7 @@ public class Book extends BaseTimeEntity {
 	@Column(name = "book_state")
 	private BookState bookState;
 
-	@ManyToOne
+	@ManyToOne(fetch= FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
