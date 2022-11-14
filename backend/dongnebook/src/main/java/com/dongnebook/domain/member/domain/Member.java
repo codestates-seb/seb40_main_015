5f0673b8d763dbbb6b27b7997286292b356df095
SJUANS <lovejuan95@hanmail.net>
2022-11-14 12:14:44 +0900

    Refactor : Member 엔티티 테이블/칼럼명 수정, accesslevel-protected 추가 MemberRegisterRequest dto 유효성 검사 추가

--- backend/dongnebook/src/main/java/com/dongnebook/domain/member/domain/Member.java
+++ backend/dongnebook/src/main/java/com/dongnebook/domain/member/domain/Member.java
@@ -7,10 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
 
 import com.dongnebook.domain.member.dto.Request.MemberRegisterRequest;
 import com.dongnebook.domain.model.Location;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,51 +20,52 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Table(name = "member")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "memberId", updatable = false)
-	private Long memberId;
+	@Column(name = "member_id", updatable = false)
+	private Long id;
 
-	@Column(name = "id", nullable = false, unique = true)
-	private String id;
+	@Column(name = "user_id", nullable = false, unique = true)
+	private String userId;
 
+	@Min(value = 8)
 	@Column(name = "password", nullable = false)
 	private String password;
 
-	@Column(name = "name", nullable = false)
-	private String name;
+	@Column(name = "nickname", nullable = false)
+	private String nickname;
 
 	@Embedded
 	private Location location;
 
-	@Column(name = "avatarUrl")
+	@Column(name = "avatar_url")
 	private String avatarUrl;
 
-	@Column(name = "AvgGrade")
-	private Long AvgGrade;
+	@Column(name = "avg_grade")
+	private Long avgGrade;
 
-	@Column(name = "totalBookCount")
+	@Column(name = "total_book_count")
 	private Long totalBookCount;
 
 	@Builder
-	public Member(String id, String password, String name, Location location, String avatarUrl, Long avgGrade,
+	public Member(String userId, String password, String nickname, Location location, String avatarUrl, Long avgGrade,
 		Long totalBookCount) {
-		this.id = id;
+		this.userId = userId;
 		this.password = password;
-		this.name = name;
+		this.nickname = nickname;
 		this.avatarUrl = avatarUrl;
 		this.location = location;
-		this.AvgGrade = avgGrade;
+		this.avgGrade = avgGrade;
 		this.totalBookCount = totalBookCount;
 	}
 
 	public static Member create(MemberRegisterRequest memberRegisterRequest) {
 		return Member.builder()
-			.id(memberRegisterRequest.getId())
-			.name(memberRegisterRequest.getName())
+			.userId(memberRegisterRequest.getUserId())
+			.nickname(memberRegisterRequest.getNickname())
 			.password(memberRegisterRequest.getPassword())
 			.build();
 	}
