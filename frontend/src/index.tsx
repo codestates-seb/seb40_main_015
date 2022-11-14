c7f4e2215244c29758f548904cbfc15da10e8c8e
2pandi <coder.2pandi@gmail.com>
2022-11-14 11:44:58 +0900

    [FE] Feat : 공통 버튼 생성, 테마 컬러 추가 (#21)
    * chore: gitignore 파일 DevApp 적용
    
    * feat: DevApp 렌더링 적용
    
    * feat: theme 색상 팔레트 추가
    
    * feat: Button 컴포넌트 추가
    
    * feat: 버튼 포인터 설정
    
    * AppDev 파일 삭제

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import AppDev from './AppDev';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/styles/theme';
+import DevApp from './DevApp';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
@@ -14,7 +14,7 @@ root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<ThemeProvider theme={theme}>
-				<AppDev />
+				<DevApp />
 			</ThemeProvider>
 		</Provider>
 	</React.StrictMode>,
