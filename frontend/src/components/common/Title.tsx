03285aec62b7b517eb7a1c334495b96c6413ffff
jooyong-boo <qnwndyd159@naver.com>
2022-11-14 16:06:41 +0900

    feat: Title 컴포넌트 구현

---
+++ frontend/src/components/common/Title.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Title = ({ text }: { text: string }) => {
+	return <Text>{text}</Text>;
+};
+
+export default Title;
+
+const Text = styled.p`
+	width: 100%;
+	font-size: 2.5rem;
+	text-align: center;
+	padding-bottom: 1rem;
+	border-bottom: 1px solid #a7a7a7;
+`;
