b5ef91e6c45987dfa0c71ab69a206630789070a5
Withlaw <98410418+Withlaw@users.noreply.github.com>
2022-11-14 14:08:10 +0900

    Fix: 라우팅 탭 부분  제거 (#35)
    Co-authored-by: Withlaw <Withlaw@github.io>

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -7,27 +7,21 @@ import LayoutTemp from '../pages/LayoutTemp';
 import Loading from '../components/Loading';
 import {
 	MainPage,
-	BookingListPage,
 	BooksBookingPage,
 	BooksCreatePage,
 	BooksDetailPage,
-	BooksListPage,
 	BooksPage,
 	BooksRentalPage,
 	BooksSearchPage,
 	ChatsPage,
 	HistoryPage,
-	LendHistoryPage,
 	LoginPage,
 	MerchantPage,
 	NoticePage,
 	ProfileEditPage,
 	ProfilePage,
-	RentHistoryPage,
 	ReviewCreatePage,
-	ReviewListPage,
 	SignupPage,
-	WishListPage,
 	LoginOnly,
 } from '../pages';
 
@@ -49,31 +43,18 @@ function Router() {
 								path="books/:bookId/booking"
 								element={<BooksBookingPage />}
 							/>
-							<Route path="profile" element={<ProfilePage />}>
-								{/* <Route path="wish-list" element={<WishListPage />} /> */}
-								<Route index element={<WishListPage />} />
-								<Route path="booking-list" element={<BookingListPage />} />
-							</Route>
+							<Route path="profile" element={<ProfilePage />} />
 							<Route path="profile/edit" element={<ProfileEditPage />} />
-							<Route path="history" element={<HistoryPage />}>
-								{/* <Route path='rent' element={<RentHistoryPage />} /> */}
-								<Route index element={<RentHistoryPage />} />
-								<Route path="lend" element={<LendHistoryPage />} />
-							</Route>
+							<Route path="history" element={<HistoryPage />} />
 							<Route path="review/create" element={<ReviewCreatePage />} />
 							<Route path="chats" element={<ChatsPage />} />
 							<Route path="notice" element={<NoticePage />} />
 						</Route>
-
 						<Route path="books" element={<BooksPage />} />
 						<Route path="books/search" element={<BooksSearchPage />} />
 						<Route path="books/:bookId" element={<BooksDetailPage />} />
-						<Route path="profile/merchant" element={<MerchantPage />}>
-							{/* merchant -> :userId ? */}
-							{/* <Route path="books-list" element={<BooksListPage />} /> */}
-							<Route index element={<BooksListPage />} />
-							<Route path="review-list" element={<ReviewListPage />} />
-						</Route>
+						<Route path="profile/merchant" element={<MerchantPage />} />
+						{/* merchant -> :userId ? */}
 					</Route>
 
 					<Route path="/login" element={<LoginPage />} />
