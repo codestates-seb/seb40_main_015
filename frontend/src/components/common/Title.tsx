c04bd75cdb751a18fea0c312fd452e1b0b76ace8
Withlaw <Withlaw@github.io>
2022-11-14 16:52:13 +0900

    design: 타이틀 로고 로직 추가

--- frontend/src/components/common/Title.tsx
+++ frontend/src/components/common/Title.tsx
@@ -1,16 +1,39 @@
 import styled from 'styled-components';
+import logo from '../../assets/image/동네북 로고 1.png';
 
-const Title = ({ text }: { text: string }) => {
-	return <Text>{text}</Text>;
+const Title = ({ text, isLogo }: { text: string; isLogo?: boolean }) => {
+	return (
+		<Text>
+			{isLogo ? (
+				<>
+					<Logo src={logo} />
+					{text}
+				</>
+			) : (
+				text
+			)}
+		</Text>
+	);
 };
 
 export default Title;
+
+const Logo = styled.img`
+	width: 50px;
+	height: 50px;
+
+	margin-right: 10px;
+`;
 
 const Text = styled.p`
 	width: 100%;
 	font-size: 2.5rem;
 	text-align: center;
-	padding-bottom: 1rem;
+	padding: 1rem 0;
 	margin-bottom: 1rem;
 	border-bottom: 1px solid #a7a7a7;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
--- frontend/src/pages/BooksPage.tsx
+++ frontend/src/pages/BooksPage.tsx
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 import BookItem from '../components/Books/BookItem';
 import Button from '../components/common/Button';
+import Title from '../components/common/Title';
 import HeaderApp from '../components/Header/HeaderApp';
+import logo from '../assets/image/동네북 로고 1.png';
 
 const BooksPage = () => {
 	return (
 		<Main>
-			<HeaderApp />
+			<TitleWrapper>
+				<Title text="동네북" isLogo={true} />
+			</TitleWrapper>
 			<BtnWrapper>
 				{/* <LongButton /> */}
 				<Button>책 등록하기</Button>
@@ -24,6 +28,12 @@ const Main = styled.div`
 	flex-direction: column;
 `;
 
+const TitleWrapper = styled.div``;
+
+const Logo = styled.img`
+	width: 50px;
+	height: 50px;
+`;
 const BtnWrapper = styled.div`
 	display: flex;
 	width: 100vh;
