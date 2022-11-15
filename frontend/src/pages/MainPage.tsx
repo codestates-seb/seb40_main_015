ce4103cabf69641bb7aaef95e32e76aebe623d27
anjisu <guajs0818@gmail.com>
2022-11-15 16:05:04 +0900

    hero section 구현중

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,15 @@
+import styled from 'styled-components';
+import Layout from './Layout';
+
 function MainPage() {
-	return <h1>main page</h1>;
+	return (
+		<Layout>
+			<h1>main page</h1>;
+		</Layout>
+	);
 }
+const Layout = styled.div`
+	background-color: #016241;
+`;
 
 export default MainPage;
--- frontend/src/pages/ProfileEditPage.tsx
+++ frontend/src/pages/ProfileEditPage.tsx
@@ -4,7 +4,6 @@ import userImage from '../assets/image/user.png';
 import Title from '../components/common/Title';
 import Button from '../components/common/Button';
 import { HiOutlinePencilAlt } from 'react-icons/hi';
-import { useNavigate } from 'react-router-dom';
 import Modal from '../components/common/Modal';
 
 function ProfileEditPage() {
