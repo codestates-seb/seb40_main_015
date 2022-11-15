953f5287f66ad0d9a1d8ce85c2d09020d2abb2d0
이성준 <thwn40@naver.com>
2022-11-15 15:50:37 +0900

    [BE] Feat : 책 상세조회 기능 추가 (#57)
    * Feat : 책 상세조회 기능 추가
    
    * Refactor : @Transaction 추가
    
    * Refactor : FetchType.Lazy 추가

---
+++ backend/src/main/java/com/dongnebook/domain/book/dto/response/BookResponse.java
@@ -0,0 +1,37 @@
+package com.dongnebook.domain.book.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.dongnebook.domain.book.domain.BookState;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.querydsl.core.annotations.QueryProjection;
+
+
+import lombok.Getter;
+
+@Getter
+public class BookResponse {
+
+	private Long bookId;
+	private String title;
+	private String publisher;
+	private Integer rentalFee;
+	private String content;
+	private String state;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	private LocalDateTime rentalStart;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	private LocalDateTime rentalEnd;
+
+	@QueryProjection
+	public BookResponse(Long bookId, String title, String publisher, Integer rentalFee, String content, BookState state
+		) {
+		this.bookId = bookId;
+		this.title = title;
+		this.publisher = publisher;
+		this.rentalFee = rentalFee;
+		this.content = content;
+		this.state = state.getMessage();
+
+	}
+}
