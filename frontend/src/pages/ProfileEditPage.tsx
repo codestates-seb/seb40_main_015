3eeaa730f410e001a4d44f0833284ee421864df3
anjisu <guajs0818@gmail.com>
2022-11-15 10:44:27 +0900

    마이페이지 수정 페이지 인풋창 수정

--- frontend/src/pages/ProfileEditPage.tsx
+++ frontend/src/pages/ProfileEditPage.tsx
@@ -3,18 +3,26 @@ import styled from 'styled-components';
 import userImage from '../assets/image/user.png';
 import Title from '../components/common/Title';
 import Button from '../components/common/Button';
+import { HiOutlineCheckCircle } from 'react-icons/hi';
+import { useNavigate } from 'react-router-dom';
+
 function ProfileEditPage() {
+	const navigate = useNavigate();
+	const handleModal = () => {
+		navigate('/profile/edit');
+	};
+
 	return (
 		<Layout>
 			<Title text="내 정보 수정하기" />
 			<ProfileBox>
 				<img src={userImage} alt="dummy" width={260} height={300} />
 				<p className="username">닉네임</p>
-				<input placeholder="닉네임을 작성하세요" />
+				<input placeholder="프론트엔드" />
 				<p className="place">내 동네 설정</p>
-				<input placeholder="주거래 지역 설정을 하세요" />
+				<input placeholder="서울시 강남구" disabled={true} />
 				<Button className="Button" fontSize={'small'}>
-					정보 수정
+					완료
 				</Button>
 			</ProfileBox>
 		</Layout>
@@ -31,11 +39,21 @@ const Layout = styled.div`
 	text-align: center;
 
 	.username {
-		padding-top: 1.25rem;
+		padding-top: 2rem;
+		padding-bottom: 0.5rem;
+	}
+
+	input {
+		width: 250px;
+		margin-top: 10;
+		padding: 5px;
+		border-radius: 10px;
+		border: 0.5px solid grey;
 	}
 
 	.place {
 		padding-top: 1.25rem;
+		padding-bottom: 0.5rem;
 	}
 `;
 
@@ -48,7 +66,9 @@ const ProfileBox = styled.div`
 	border: 1px solid #eaeaea;
 
 	.Button {
-		margin-top: 1.25rem;
+		margin-top: 2rem;
+	}
+	.input {
 	}
 `;
 
