96babb01ddae0d728b67bee9bda77eecf600c04b
SJUANS <99381863+SJUANS@users.noreply.github.com>
2022-11-16 15:01:35 +0900

    [BE] Feat : 유효성 검증과 중복확인을 포함한 회원가입 기능 구현 (#74)
    * [BE] Feat : 회원가입시 아이디 중복 확인
    
    * [BE] Feat : 회원가입시 아이디, 닉네임 중복 확인
    
    * [BE] Feat : 회원가입 로직 작성중
    
    * [BE] Feat : 회원가입 로직에 Builder 패턴 적용중
    
    * [BE] Feat : 회원가입 로직에 URI 작성중
    
    * [BE] Feat : 비밀번호 암호화해서 저장
    
    * [BE] Feat : 회원가입 후 members/{id} uri 자원 생성
    
    * [BE] Refactor: 아이디 유효성 검사 정규표현식 수정
    
    * [BE] Feat : 회원등록 & responsebody에 고유Id 반환
    
    * [BE] Refactor : 아이디 유효성 검증 정규표현식 수정
    
    * [BE] Feat : 아이디/닉네임 중복검사 API 작성(*builder 패턴 사용 시도->안됨)
    
    * [BE] Feat : 아이디/닉네임 중복검사 API 구현 완료
    
    * [BE] Refactor: 중복 회원가입 관련 exception과 ErrorCode 삭제(사유: 회원가입시 구현한 API로 따로 체크함)
    
    * [BE] Refactor : Member에서 Setter 삭제하고 비밀번호 암호화 메서드 MemberService에 추가 / 회원가입 API에서 responseentity에 넣는 dto 삭제하고 바로 id 삽입
    
    * [BE] Refactor : application.yml 수정
    
    * [BE] Refactor : 비밀번호 암호화 로직 변경(Setter 제거), 회원가입 ResponseEntity 생성 구조 변경(MemberIdResponse 제거)
    
    Co-authored-by: 이성준 <thwn40@naver.com>

--- backend/src/main/java/com/dongnebook/domain/member/application/MemberService.java
+++ backend/src/main/java/com/dongnebook/domain/member/application/MemberService.java
@@ -1,5 +1,7 @@
 package com.dongnebook.domain.member.application;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.dongnebook.domain.member.domain.Member;
@@ -9,17 +11,44 @@ import com.dongnebook.domain.member.repository.MemberRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Getter
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-
 	private final MemberRepository memberRepository;
+	private PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
+	@Transactional
 	public Long create(MemberRegisterRequest memberRegisterRequest) {
-		Member member = Member.create(memberRegisterRequest);
-		return memberRepository.save(member).getId();
+		Member member = Member.builder()
+					.userId(memberRegisterRequest.getUserId())
+					.nickname(memberRegisterRequest.getNickname())
+					.password(passwordEncoder.encode(memberRegisterRequest.getPassword()))
+					.build();
+		Long id = memberRepository.save(member).getId();
+
+		return id;
+	}
+	@Transactional
+	public String encodePassword(String password){
+		System.out.println(password);
+		String encodedPassword = passwordEncoder.encode(password);
+		System.out.println(encodedPassword);
+		return encodedPassword;
+	}
+
+	@Transactional(readOnly = true)
+	public boolean checkUserIdDuplication(String userId) {
+		boolean userIdDuplicate = memberRepository.existsByUserId(userId);
+		return userIdDuplicate;
+	}
+
+	@Transactional(readOnly = true)
+	public boolean checkNicknameDuplication(String nickname) {
+		boolean nicknameDuplicate = memberRepository.existsByNickname(nickname);
+		return nicknameDuplicate;
 	}
 }
