a0faab828b84c1ad26d0c95ad9f74c672851ac7e
2pandi <coder.2pandi@gmail.com>
2022-11-16 10:32:11 +0900

    [FE] feat : 회원가입 페이지 뷰 완성 (#75)
    * design: 폰트 변경으로 로그인 페이지 CSS 조정
    
    * design: add cursor option
    
    * feat: LoginForm preventDefault 추가
    
    * feat: Clause 컴포넌트 작성
    
    * refactor: LinkToSignUp 컴포넌트 공통 컴포넌트로 리팩토링
    
    * feat: SignupPage LinkToSign props 설정
    
    * feat: SignUpForm 컴포넌트 작성
    
    * feat: SignupPage 완성

---
+++ frontend/src/components/common/LinkToSign.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+type LinkToSignProps = {
+	message: string;
+	link: string;
+	linkText: string;
+};
+
+const LinkToSign = (props: LinkToSignProps) => {
+	const { message, link, linkText } = props;
+	return (
+		<StyledLinkToSign>
+			{message}{' '}
+			<Link to={link}>
+				<StyledSpan>{linkText}</StyledSpan>
+			</Link>
+		</StyledLinkToSign>
+	);
+};
+
+const StyledLinkToSign = styled.div`
+	margin: 1.5rem;
+	text-align: center;
+`;
+
+const StyledSpan = styled.span`
+	color: ${props => props.theme.colors.buttonGreen};
+	font-weight: bold;
+`;
+
+export default LinkToSign;
