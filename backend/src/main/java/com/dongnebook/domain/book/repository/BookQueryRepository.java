8eadb25466a5a35cb2088b105b38374574c639ae
이성준 <thwn40@naver.com>
2022-11-16 11:32:07 +0900

    [BE] Feat : 도서 검색 - 지도에 구획별 책 갯수를 뿌려준다. v1 (#73)
    * Feat : 도서 검색 - 지도에 구획별 책 갯수를 뿌려준다.
    
    * Refactor : Set메소드 수정

--- backend/src/main/java/com/dongnebook/domain/book/repository/BookQueryRepository.java
+++ backend/src/main/java/com/dongnebook/domain/book/repository/BookQueryRepository.java
@@ -1,14 +1,17 @@
 package com.dongnebook.domain.book.repository;
 
 import static com.dongnebook.domain.book.domain.QBook.*;
-import static com.dongnebook.domain.member.domain.QMember.*;
+
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.dongnebook.domain.book.dto.request.SectorBookCountRequest;
 import com.dongnebook.domain.book.dto.response.BookDetailResponse;
 import com.dongnebook.domain.book.dto.response.QBookDetailResponse;
 import com.dongnebook.domain.book.dto.response.QBookResponse;
 import com.dongnebook.domain.member.dto.response.QBookDetailMemberResponse;
+import com.dongnebook.domain.model.Location;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -41,5 +44,30 @@ public class BookQueryRepository {
 			.fetchOne();
 
 	}
+
+	/**
+	 * 위도 0.00001 = 1.1m 0.00227 = 250m
+	 * 경도 0.00001 = 0.9m 0.00277 = 250m
+	 * 보여주고 싶은 정보 -> 현재 위치가 중앙인 가로 세로 500m 짜리 정사각형에서 마커를 클러스터링 해서 보여주기
+	 * db 에서 근방에 있는 책들 (pk와 주소) 모두 가져오기  -> 어플리케이션단에서 그룹핑하기
+	 * db 에서 그룹핑해서 가져오기 -> 그럼 select 쿼리가 9번??
+
+	 */
+	public List<Location> getSectorBookCounts(SectorBookCountRequest sectorBookCountRequest) {
+
+		String bookTitle = sectorBookCountRequest.getBookTitle();
+		List<Double> LatRange = sectorBookCountRequest.latRangeList();
+		List<Double> LonRange = sectorBookCountRequest.lonRangeList();
+
+		return jpaQueryFactory.select(book.location)
+			.from(book)
+			.where(book.location.latitude.between(LatRange.get(3), LatRange.get(0))
+				.and(book.location.longitude.between(LonRange.get(0), LonRange.get(3))
+					.and(book.title.contains(bookTitle))))
+			.fetch();
+
+
+	}
+
 
 }
