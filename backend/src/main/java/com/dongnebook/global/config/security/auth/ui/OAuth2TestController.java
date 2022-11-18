784b0a5393c0e6d8e84711e0f18e1050b81e3cd1
SJUANS <lovejuan95@hanmail.net>
2022-11-18 22:39:30 +0900

    [BE] Refactor: SSR 방식 OAuth2 자동인증 테스트용 컨트롤러와 페이지 작성

--- backend/src/main/java/com/dongnebook/global/config/security/auth/ui/OAuth2TestController.java
+++ backend/src/main/java/com/dongnebook/global/config/security/auth/ui/OAuth2TestController.java
@@ -1,7 +1,9 @@
 package com.dongnebook.global.config.security.auth.ui;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class OAuth2TestController {
     @GetMapping("/hello-oauth2")
     public String home() {
