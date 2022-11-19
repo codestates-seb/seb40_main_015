6000abadc3707250409775e4436a9fca9af32652
이성준 <thwn40@naver.com>
2022-11-19 13:11:56 +0900

    [BE] Feat : 회원 열람 api, 회원 변경 api 구현 (#115)
    * Feat : 현재 사용자 불러오는 기능 추가, 검색 기본 페이지용 조회 추가
    
    * Feat : 회원정보 열람 api, 회원정보 수정 api 추가

---
+++ backend/src/main/java/com/dongnebook/domain/member/dto/request/MerchantSearchRequest.java
@@ -0,0 +1,40 @@
+package com.dongnebook.domain.member.dto.request;
+
+import static com.dongnebook.domain.book.domain.QBook.*;
+import static com.dongnebook.domain.member.domain.QMember.*;
+
+import java.util.List;
+
+import com.dongnebook.domain.member.domain.QMember;
+import com.dongnebook.domain.model.Location;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import lombok.Getter;
+
+@Getter
+public class MerchantSearchRequest {
+
+	private	Double longitude;
+	private Double latitude;
+	private Integer sector;
+
+	public MerchantSearchRequest(Double longitude, Double latitude, Integer sector) {
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.sector = sector;
+	}
+
+	public BooleanExpression sectorBetween() {
+		int count = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				count++;
+				if (sector == count) {
+					return member.location.latitude.between(Location.latRangeList(latitude).get(i + 1), Location.latRangeList(latitude).get(i))
+						.and(member.location.longitude.between(Location.lonRangeList(longitude).get(j), Location.lonRangeList(longitude).get(j + 1)));
+				}
+			}
+		}
+		return null;
+	}
+}
--- backend/src/main/java/com/dongnebook/domain/member/dto/response/MemberResponse.java
+++ backend/src/main/java/com/dongnebook/domain/member/dto/response/MemberResponse.java
@@ -1,5 +1,18 @@
 package com.dongnebook.domain.member.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
+
+import lombok.Getter;
+
+@Getter
 public class MemberResponse {
 
+	private Long id;
+	private String name;
+
+	@QueryProjection
+	public MemberResponse(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 }
