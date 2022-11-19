f396d1bb2ce888a52e210b59ad0c96245f056811
SJUANS <lovejuan95@hanmail.net>
2022-11-19 14:02:03 +0900

    [BE] Fix : 유효하지 않은 API 키 재발급

--- backend/src/main/java/com/dongnebook/global/config/security/auth/SecurityConfiguration.java
+++ backend/src/main/java/com/dongnebook/global/config/security/auth/SecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -60,7 +61,7 @@ public class SecurityConfiguration {
 				.apply(new CustomFilterConfigurer())
 				.and()
 				.authorizeHttpRequests(authorize -> authorize
-						.anyRequest().authenticated() // 테스트 위해 모든 리퀘스트에 대한 권한 인증 필요로 바꿈
+						.anyRequest().authenticated()
 				)
 				.oauth2Login(withDefaults());
 		return http.build();
