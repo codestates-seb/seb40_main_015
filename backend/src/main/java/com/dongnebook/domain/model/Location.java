2517869ed6bc43d05ce90501aa1ada8dcbe5f951
이성준 <thwn40@naver.com>
2022-11-19 18:28:00 +0900

    Refactor : 지도 가로세로 1km 로 수정 (#121)

--- backend/src/main/java/com/dongnebook/domain/model/Location.java
+++ backend/src/main/java/com/dongnebook/domain/model/Location.java
@@ -40,25 +40,26 @@ public class Location {
 
 	public List<Double> latRangeList(){
 		Double centralLat = this.latitude;
-		return Arrays.asList(centralLat + 0.00227, centralLat + 0.00075, centralLat - 0.00075,
-			centralLat - 0.00227);
+		return Arrays.asList(centralLat + 0.00454, centralLat + 0.00151, centralLat - 0.00151,
+			centralLat - 0.00454);
 	}
 
 	public List<Double> lonRangeList(){
 		Double centralLon = this.longitude;
-		return Arrays.asList(centralLon - 0.00277, centralLon - 0.00093, centralLon + 0.00093,
-			centralLon + 0.00277);
+		return Arrays.asList(centralLon - 0.00554, centralLon - 0.00186, centralLon + 0.00186,
+			centralLon + 0.00554);
 	}
 
 	public static List<Double> latRangeList(Double latitude){
-		return Arrays.asList(latitude + 0.00227, latitude + 0.00075, latitude - 0.00075,
-			latitude - 0.00227);
+		return Arrays.asList(latitude + 0.00454, latitude + 0.00151, latitude - 0.00151,
+			latitude - 0.00454);
 	}
 
 	public static List<Double> lonRangeList(Double longitude){
-		return Arrays.asList(longitude - 0.00277, longitude - 0.00093, longitude + 0.00093,
-			longitude + 0.00277);
+		return Arrays.asList(longitude - 0.00554, longitude - 0.00186, longitude + 0.00186,
+			longitude + 0.00554);
 	}
+
 
 
 
