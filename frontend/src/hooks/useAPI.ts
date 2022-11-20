d3c0d2ad831b8721a331ac6bf56b79e244517709
Withlaw <98410418+Withlaw@users.noreply.github.com>
2022-11-20 23:39:54 +0900

    [FE] fix: redux store reducer 분리 (#132)
    * Fix: 라우팅 탭 부분  제거
    
    * Feat: 메인페이지 스타일 1
    
    * Feat: 메인페이지 스타일 2
    
    * design: 타이틀 로고 로직 추가
    
    * design: 메인페이지 스타일 완성1
    
    * Fix: 레이아웃깨짐 수정
    
    * feat: 도서상세페이지 html/css 구현
    
    * feat: 홈 화면에서 글 클릭시 책 상세조회 페이지로 넘어가는 기능 추가
    
    * feat: axios 기본설정
    
    * store reducer 수정 (주석처리함)
    
    * fix: notification dispatch 수정
    
    Co-authored-by: Withlaw <Withlaw@github.io>
    Co-authored-by: 2pandi <coder.2pandi@gmail.com>

--- frontend/src/hooks/useAPI.ts
+++ frontend/src/hooks/useAPI.ts
@@ -2,17 +2,6 @@ import axios from 'axios';
 import { BASE_URL } from '../constants/constants';
 import { useAppSelector } from '../redux/hooks';
 
-// axios({
-// 	method: 'post',
-// 	url: BASE_URL + '/auth/login',
-// 	withCredentials: true,
-// 	data: payload,
-// 	timeout: 5000,
-// 	headers: { ContentType: 'application/json' },
-// })
-// 	.then(data => console.log('res: ', data))
-// 	.then(err => console.error(err));
-
 const useAPI = () => {
 	const { accessToken } = useAppSelector(state => state.loginInfo);
 	const config = {
