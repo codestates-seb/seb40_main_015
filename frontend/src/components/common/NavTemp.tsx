8dfbaa01ff9092820af8906d71640a2ce01817f8
Withlaw <98410418+Withlaw@users.noreply.github.com>
2022-11-21 13:36:03 +0900

    [FE] feat: 로그인 관련 성공 및 에러 메시지 구현 (#135)
    * Fix: 라우팅 탭 부분  제거
    
    * Feat: 메인페이지 스타일 1
    
    * Feat: 메인페이지 스타일 2
    
    * design: 타이틀 로고 로직 추가
    
    * design: 메인페이지 스타일 완성1
    
    * Fix: 레이아웃깨짐 수정
    
    * feat: 도서상세페이지 html/css 구현
    
    * feat: 홈 화면에서 글 클릭시 책 상세조회 페이지로 넘어가는 기능 추가
    
    * feat: axios 기본설정
    
    * store reducer 수정 (주석처리함)
    
    * redux store reducer 분리
    
    * feat: login 미입력 오류 및 로그인 실패 ui 구현
    
    * feat: 비로그인 상태에서 로그인전용페이지 접근시 알림 출력
    
    Co-authored-by: Withlaw <Withlaw@github.io>

--- frontend/src/components/common/NavTemp.tsx
+++ frontend/src/components/common/NavTemp.tsx
@@ -8,8 +8,10 @@ import {
 	HiHome,
 } from 'react-icons/hi';
 import home from '../../assets/image/logo4.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import theme from '../../styles/theme';
+import { useAppSelector } from '../../redux/hooks';
+import useNotify, { useNotifyHook } from '../../hooks/useNotify';
 
 interface MenuProps {
 	id: number;
@@ -18,7 +20,10 @@ interface MenuProps {
 	path: string;
 	selected: boolean;
 }
-
+const Unrestricted = (id: number) => {
+	// 로그인 버튼이 따로 없어서 마이페이지는 일단 로그인 요청 페이지로 이동시킴
+	return id === 0 || id === 2 || id === 4;
+};
 const NavTemp = () => {
 	const [menus, setMenus] = useState<MenuProps[]>([
 		{
@@ -58,7 +63,15 @@ const NavTemp = () => {
 		},
 	]);
 
+	// login 체크 로직
+	const notify = useNotifyHook();
+	const isLogin = useAppSelector(state => state.loginInfo.isLogin);
+	const location = useLocation();
+
 	const handleChangeMenu = (id: number): void => {
+		// login 체크 로직
+		if (!Unrestricted(id) && !isLogin) notify('로그인이 필요합니다.');
+
 		const newMenus = menus.map(menu =>
 			menu.id === id
 				? { ...menu, selected: true }
@@ -72,7 +85,9 @@ const NavTemp = () => {
 			{menus.map(menu => {
 				const { id, icon, text, selected, path } = menu;
 				return (
-					<Link to={path} key={id}>
+					<Link
+						to={Unrestricted(id) || isLogin ? path : location.pathname}
+						key={id}>
 						<Box selected={selected} onClick={() => handleChangeMenu(id)}>
 							{icon}
 							{text}
