b8b05210c5aded1a7e22cf6d8c96420db3a6bfcd
2pandi <coder.2pandi@gmail.com>
2022-11-22 11:30:52 +0900

    feat: 더미 데이터 추가

--- frontend/src/pages/NoticePage.tsx
+++ frontend/src/pages/NoticePage.tsx
@@ -23,6 +23,12 @@ const dummy = [
 	{ id: 0, type: 'reservation', title: '자바의 정석' },
 	{ id: 1, type: 'return', title: '모비딕' },
 	{ id: 2, type: 'rental', title: '리팩터링 2판' },
+	{
+		id: 3,
+		type: 'merchantCancellation',
+		title: '한 권으로 읽는 컴퓨터 구조와 프로그래밍',
+	},
+	{ id: 4, type: 'residentCancellation', title: '누가 내 머리에 똥쌌어?' },
 ];
 
 export default NoticePage;
