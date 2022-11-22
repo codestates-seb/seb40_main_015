8eacd4cedf09c78737509defccf6c193b081ddfe
jujang <jeongookjang@naver.com>
2022-11-22 12:12:20 +0900

    feat: 빌려준 책 조회 기능 구현 (#161)

--- backend/src/main/java/com/dongnebook/domain/rental/repository/RentalRepository.java
+++ backend/src/main/java/com/dongnebook/domain/rental/repository/RentalRepository.java
@@ -3,5 +3,9 @@ package com.dongnebook.domain.rental.repository;
 import com.dongnebook.domain.rental.domain.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+
 public interface RentalRepository extends JpaRepository<Rental,Long> {
+    ArrayList<Rental> findAllByMerchantIdOrderByIdDesc(Long merchantId);
+
 }
