c3849635fa94a3efe9deae42ab66791ff23634ed
jooyong-boo <qnwndyd159@naver.com>
2022-11-24 00:49:02 +0900

    feat: index 유무에따라 queryString 포함여부 구현

--- frontend/src/api/mypage.ts
+++ frontend/src/api/mypage.ts
@@ -1,52 +1,52 @@
-
 import useAPI from '../hooks/useAPI';
-
-
 
 // 마이페이지 유저정보 및 찜목록 요청 getPickBookLists
 interface Member {
 	memberId: number;
 	name: string;
-	location:{
+	location: {
 		lat: string | number;
 		lon: string | number;
-} | null,
+	} | null;
 	address: string | null;
 	totalBookCount: number;
 	avatarUrl: string | null;
 }
 
 interface PickBook {
-	bookId:      string;
-	title:       string;
-	rentalFee:   number;
-	status:      string;
+	bookId: string;
+	title: string;
+	rentalFee: number;
+	status: string;
 }
-
 
 export const useMypageAPI = () => {
 	const api = useAPI();
 
 	// 상인정보용 도서 목록 조회(주용님)
-	const getMerchantBookLists = (id: string | undefined) =>
-	api.get(`/member/${id}/books`).then(res => res.data);
-	
+	const getMerchantBookLists = (
+		id: string | undefined,
+		index?: string | undefined,
+	) => {
+		if (index) {
+			return api
+				.get(`/member/${id}/books`, { params: { index } })
+				.then(res => res.data);
+		} else {
+			return api.get(`/member/${id}/books`).then(res => res.data);
+		}
+	};
 
 	// 마이페이지 - 회원정보 열람(주용님)
-		const getMemberInfo = async (id: string | undefined) =>
-		await api.get(`/member/${id}`).then(res => res.data)
-
+	const getMemberInfo = async (id: string | undefined) =>
+		await api.get(`/member/${id}`).then(res => res.data);
 
 	// 마이페이지 - 회원정보 열람
-			const getMyInfo = async (id: string | undefined) =>
-			await api.get<Member>(`/member/${id}`).then(res => res.data);
-		
-			
+	const getMyInfo = async (id: string | undefined) =>
+		await api.get<Member>(`/member/${id}`).then(res => res.data);
+
 	//마이페이지 - 찜목록
-		const getPickBookList = () => 
-			api.get<PickBook>(`/dibs`);
-	
-	
+	const getPickBookList = () => api.get<PickBook>(`/dibs`);
 
 	// 마이페이지 - 회원정보 수정
 	const getFixMemberInfo = async (id: string) => {
@@ -58,7 +58,6 @@ export const useMypageAPI = () => {
 			return err;
 		}
 	};
-
 
 	// 마이페이지 - 예약목록(API 명세서 나오지 않음)
 	// const getReservationBookLists = async () => {
@@ -81,7 +80,7 @@ export const useMypageAPI = () => {
 			return err;
 		}
 	};
-	
+
 	// 사진 등록(지구)
 	const axiosAddPhoto = async () => {
 		try {
@@ -93,10 +92,6 @@ export const useMypageAPI = () => {
 		}
 	};
 
-	
-
-	
-
 	return {
 		getMyInfo,
 		getMemberInfo,
@@ -104,6 +99,6 @@ export const useMypageAPI = () => {
 		getPickBookList,
 		axiosCancleReservation,
 		axiosAddPhoto,
-		getMerchantBookLists
+		getMerchantBookLists,
 	};
 };
