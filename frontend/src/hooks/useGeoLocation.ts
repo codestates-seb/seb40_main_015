4708dc21dfc46a16569709aab62818a9e841bdae
jooyong-boo <qnwndyd159@naver.com>
2022-11-26 02:01:48 +0900

    refactor: key값 수정 반복되는 로직 개선

--- frontend/src/hooks/useGeoLocation.ts
+++ frontend/src/hooks/useGeoLocation.ts
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 
 function useGeoLocation() {
 	const [current, setCurrent] = useState<any>({
-		La: 37.39277543968578,
-		Ma: 126.93629486796846,
+		lat: 37.39277543968578,
+		lon: 126.93629486796846,
 	});
 	const handleCurrentLocationMove = () => {
 		let lat = 0;
@@ -15,7 +15,7 @@ function useGeoLocation() {
 			position => {
 				lat = position.coords.latitude; // 위도
 				lon = position.coords.longitude; // 경도
-				setCurrent({ La: lon, Ma: lat });
+				setCurrent({ lat, lon });
 			},
 			null,
 			options,
@@ -23,20 +23,7 @@ function useGeoLocation() {
 	};
 
 	useEffect(() => {
-		let lat = 0;
-		let lon = 0;
-		let options = {
-			enableHighAccuracy: true,
-		};
-		navigator.geolocation.getCurrentPosition(
-			position => {
-				lat = position.coords.latitude; // 위도
-				lon = position.coords.longitude; // 경도
-				setCurrent({ La: lon, Ma: lat });
-			},
-			null,
-			options,
-		);
+		handleCurrentLocationMove();
 	}, []);
 
 	return [current, setCurrent, handleCurrentLocationMove];
