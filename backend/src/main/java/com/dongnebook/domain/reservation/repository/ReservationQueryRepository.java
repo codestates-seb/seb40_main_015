bf0fa350c9f6b07e76d4e22cca36b36eacdbfded
jujang <jeongookjang@naver.com>
2022-11-27 15:26:14 +0900

    [BE] Feat: 도서 예약목록 조회 기능 구현 (#205)
    * feat: 도서 예약목록 조회 기능 구현
    
    * feat: 도서 예약목록 조회 기능 구현 - DB 저장 데이터명 수정

--- backend/src/main/java/com/dongnebook/domain/reservation/repository/ReservationQueryRepository.java
+++ backend/src/main/java/com/dongnebook/domain/reservation/repository/ReservationQueryRepository.java
@@ -1,8 +1,15 @@
 package com.dongnebook.domain.reservation.repository;
 
+import com.dongnebook.domain.book.dto.response.QBookSimpleResponse;
 import com.dongnebook.domain.reservation.domain.Reservation;
+import com.dongnebook.domain.reservation.dto.request.QReservationInfoResponse;
+import com.dongnebook.domain.reservation.dto.request.QReservationResponse;
+import com.dongnebook.domain.reservation.dto.request.ReservationInfoResponse;
+import com.dongnebook.global.dto.request.PageRequest;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -24,16 +31,54 @@ public class ReservationQueryRepository {
                 .fetch();
     }
 
-//    public List<Reservation> getReservationById(Long reservationId){
-//        return jpaQueryFactory.selectFrom(reservation)
-//                .where(reservation.id.eq(reservationId))
-//                .fetch();
-//    }
-
     public Reservation getReservationById(Long reservationId){
         return jpaQueryFactory.selectFrom(reservation)
                 .where(reservation.id.eq(reservationId))
                 .fetchOne();
+    }
+
+    public SliceImpl<ReservationInfoResponse> findAllByMemberIdOrderByIdDesc(Long memberId, PageRequest pageRequest){
+
+        List<ReservationInfoResponse> reservations = jpaQueryFactory.select(new QReservationInfoResponse(
+                        new QReservationResponse(
+                            reservation.id,
+                            reservation.rentalExpectedAt
+                        ),
+                        new QBookSimpleResponse(
+                            reservation.book.id,
+                            reservation.book.title,
+                            reservation.book.rentalFee,
+                            reservation.book.ImgUrl,
+                            reservation.book.member.nickname
+                        )
+                    )
+                )
+                .from(reservation)
+                .innerJoin(reservation.book)
+                .where(ltReservationId(pageRequest.getIndex()),
+                        reservation.member.id.eq(memberId)
+                )
+                .orderBy(reservation.id.desc())
+                .limit(pageRequest.getSize() + 1)
+                .fetch();
+
+
+        boolean hasNext = false;
+
+        if(reservations.size() > pageRequest.getSize()){
+            hasNext = true;
+            reservations.remove(pageRequest.getSize().intValue());
+        }
+
+        return new SliceImpl<>(reservations, pageRequest.of(), hasNext);
+
+    }
+
+    private BooleanExpression ltReservationId(Long reservationId){
+        if(reservationId == null){
+            return null;
+        }
+        return reservation.id.lt(reservationId);
     }
 
 }
