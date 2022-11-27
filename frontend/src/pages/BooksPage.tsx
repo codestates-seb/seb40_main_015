45dad5bb3bb8a67de660c7449c747129a33fbff0
Withlaw <Withlaw@github.io>
2022-11-27 22:45:17 +0900

    feat: nav바 로그인 버튼 추가

--- frontend/src/pages/BooksPage.tsx
+++ frontend/src/pages/BooksPage.tsx
@@ -11,8 +11,12 @@ import Animation from '../components/Loading/Animation';
 
 //hooks
 import { useBooksAPI } from '../api/books';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import notify from '../utils/notify';
 
 const BooksPage = () => {
+	const { isLogin } = useAppSelector(state => state.loginInfo);
+	const dispatch = useAppDispatch();
 	const { getAllBooksListInfinite } = useBooksAPI();
 	const target = useRef<HTMLDivElement>(null);
 
@@ -47,8 +51,11 @@ const BooksPage = () => {
 			</TitleWrapper>
 
 			<BtnWrapper>
-				<LinkStyled to={'/books/create'}>
-					<Button>책 등록하기</Button>
+				<LinkStyled to={isLogin ? '/books/create' : ''}>
+					<Button
+						onClick={() => isLogin || notify(dispatch, '로그인이 필요합니다')}>
+						책 등록하기
+					</Button>
 				</LinkStyled>
 			</BtnWrapper>
 
