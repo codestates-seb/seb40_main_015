80f859bf3f89ec313070f65ff2b97ab6af3783b8
fejigu <102123710+anjigu@users.noreply.github.com>
2022-11-29 00:09:57 +0900

    feat: 마이페이지 및 내정보수정하기

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -7,12 +7,13 @@ import persistReducer from 'redux-persist/lib/persistReducer';
 import notificationReducer from './slice/notificationSlice';
 import loginInfoReducer from './slice/userSlice';
 import bookCreateReducer from './slice/bookCreateSlice';
+import userInfoReducer from './slice/userInfoSlice';
 
 const persistConfig = {
 	key: 'root',
 	version: 1,
 	storage,
-	blacklist: ['bookCreate'],
+	blacklist: ['bookCreate', 'userInfo'],
 };
 
 const loginPersistConfig = {
@@ -23,6 +24,7 @@ const loginPersistConfig = {
 const rootReducer = combineReducers({
 	notification: notificationReducer,
 	bookCreate: bookCreateReducer,
+	userInfo: userInfoReducer
 });
 
 // const persistedReducer = persistReducer(persistConfig, rootReducer);
