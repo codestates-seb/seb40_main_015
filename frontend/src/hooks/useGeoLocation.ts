b9ec83a328ff2883c26073ff977914a2d599c5f0
jooyong-boo <qnwndyd159@naver.com>
2022-11-29 03:28:48 +0900

    feat: 좌표 초기값 및 로딩 처리를 위한 값 설정

--- frontend/src/hooks/useGeoLocation.ts
+++ frontend/src/hooks/useGeoLocation.ts
@@ -1,32 +1,49 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { change } from '../redux/slice/geoLocationSlice';
+import getLocation from '../utils/getLocation';
 
-function useGeoLocation() {
-	const [current, setCurrent] = useState<any>({
-		lat: 37.3827456,
-		lon: 126.9264032,
+function useGeoLocation(type?: string) {
+	const dispatch = useDispatch();
+	const location = useSelector((state: RootState) => {
+		return state.persistedReducer.getLocation;
 	});
-	const handleCurrentLocationMove = () => {
-		let lat = 0;
-		let lon = 0;
-		var options = {
+	const [current, setCurrent] = useState<any>({
+		lat: 33.452612905667934,
+		lon: 126.57087765868735,
+	});
+	const [loading, setLoading] = useState(false);
+
+	const handleCurrentLocationMove = async () => {
+		setLoading(true);
+		let options = {
 			enableHighAccuracy: true,
+			maximumAge: 0,
 		};
-		navigator.geolocation.getCurrentPosition(
-			position => {
-				lat = position.coords.latitude; // 위도
-				lon = position.coords.longitude; // 경도
-				setCurrent({ lat, lon });
-			},
-			null,
-			options,
-		);
+
+		let position: any = await getLocation(options);
+		let lat = position.coords.latitude; // 위도
+		let lon = position.coords.longitude; // 경도
+		let result = { lat, lon };
+		dispatch(change(result));
+		setCurrent(result);
+		setLoading(false);
+	};
+
+	const setCurrentLocation = (lat: number, lon: number) => {
+		dispatch(change({ lat, lon }));
 	};
 
 	useEffect(() => {
 		handleCurrentLocationMove();
 	}, []);
 
-	return [current, setCurrent, handleCurrentLocationMove];
+	if (type === 'center') {
+		return [location, setCurrentLocation];
+	} else {
+		return [current, setCurrent, handleCurrentLocationMove, loading];
+	}
 }
 
 export default useGeoLocation;
