68c9567a00bf8b5f515225f64a68d7095cd1c563
이성준 <thwn40@naver.com>
2022-11-29 17:04:05 +0900

    [BE] Refactor : api 요구사항 수정 (#226)
    * refactor : 찜목록 무한스크롤 문제 해결
    
    * refactor : 유저이미지 기본값 수정
    
    * refactor : 거래중이랑 거래중단은 안보이게 수정
    
    * refactor : 회원정보 api nickName -> nickname
    
    * refactor : 회원 기본평점 0점으로
    
    * feat : 상세조회에 로그인한 회원이 찜했는지 안했는지 추가

--- backend/src/main/java/com/dongnebook/domain/member/application/MemberService.java
+++ backend/src/main/java/com/dongnebook/domain/member/application/MemberService.java
@@ -56,6 +56,7 @@ public class MemberService {
 			.userId(memberRegisterRequest.getUserId())
 			.nickname(memberRegisterRequest.getNickname())
 			.password(passwordEncoder.encode(memberRegisterRequest.getPassword()))
+			.avatarUrl("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
 			.build();
 
 		return memberRepository.save(member).getId();
