0f6d62a6bbc4e6489573cb78d449b4abe53e8305
2pandi <coder.2pandi@gmail.com>
2022-11-29 19:36:46 +0900

    feat: 이미지 첨부 사이즈 제한

--- frontend/src/components/BooksCreate/Photo.tsx
+++ frontend/src/components/BooksCreate/Photo.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import useGetPhotoUrl from '../../api/hooks/common/useGetPhotoUrl';
 import { useAppDispatch } from '../../redux/hooks';
 import { updateRentalInfo } from '../../redux/slice/bookCreateSlice';
+import notify from '../../utils/notify';
 import { BookInfo } from '../Books/BookElements';
 
 const Photo = () => {
@@ -16,12 +17,17 @@ const Photo = () => {
 		const formData = new FormData();
 		if (files) {
 			const fileRef = files[0];
-			setImageName(fileRef.name);
-			formData.append('img', fileRef);
-			mutate(formData, {
-				onSuccess: res =>
-					dispatch(updateRentalInfo({ key: 'imageUrl', value: res.data })),
-			});
+			const maxSize = 2 * 1024 * 1024;
+			if (fileRef.size > maxSize) {
+				notify(dispatch, '사진은 2MB 이내로 등록 가능합니다.');
+			} else {
+				setImageName(fileRef.name);
+				formData.append('img', fileRef);
+				mutate(formData, {
+					onSuccess: res =>
+						dispatch(updateRentalInfo({ key: 'imageUrl', value: res.data })),
+				});
+			}
 		}
 	};
 
@@ -31,7 +37,9 @@ const Photo = () => {
 				<label htmlFor="photo">
 					<Photicon />
 				</label>
-				<div>{imageName || 'image file'}</div>
+				<div>
+					{imageName || 'image file (2MB 미만의 파일만 등록 가능합니다.)'}
+				</div>
 				<input
 					id="photo"
 					type="file"
