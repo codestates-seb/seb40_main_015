39fb888ac78a80e58d6b7166aabe5ecd01c81c04
2pandi <coder.2pandi@gmail.com>
2022-12-01 22:25:30 +0900

    [FE] Feat: header 작업중 (#265)
    * feat: Header 뷰 작업중
    
    * feat: 선택한 탭 텍스트 컬러 설정

---
+++ frontend/src/components/common/PcVersion/Header/Header.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+interface MenuProps {
+	id: number;
+	text: string;
+	selected: boolean;
+	link: string;
+}
+
+interface BoxProps {
+	selected?: boolean;
+}
+
+const Header = () => {
+	const [menus, setMenus] = useState<MenuProps[]>([
+		{
+			id: 1,
+			text: '지도검색',
+			selected: false,
+			link: '/books/search',
+		},
+		{
+			id: 2,
+			text: '대여목록',
+			selected: false,
+			link: '/history',
+		},
+		{
+			id: 3,
+			text: '채팅',
+			selected: false,
+			link: '/chats',
+		},
+	]);
+
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		const newMenus = menus.map(menu =>
+			menu.link === pathname
+				? { ...menu, selected: true }
+				: { ...menu, selected: false },
+		);
+		setMenus(newMenus);
+		console.log(newMenus);
+	}, [pathname]);
+
+	return (
+		<Container>
+			<Left>
+				<Logo />
+				{menus.map(menu => {
+					const { id, text, selected, link } = menu;
+					return (
+						<Link to={link} key={id}>
+							<Box selected={selected}>
+								<StyledP>{text}</StyledP>
+							</Box>
+						</Link>
+					);
+				})}
+			</Left>
+			<Link to="/profile">
+				<Box selected={pathname === '/profile'}>
+					<StyledP>마이페이지</StyledP>
+				</Box>
+			</Link>
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	width: 100%;
+	height: 70px;
+	background-color: white;
+	border-bottom: 1px solid #a7a7a7;
+	padding: 10px 0;
+	position: fixed;
+	top: 0;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	z-index: 1;
+
+	@media screen and (max-width: 800px) {
+		display: none;
+	}
+`;
+
+const Left = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Box = styled.div<BoxProps>`
+	color: ${props => (props.selected ? '#26795D' : '#000000')};
+	margin: 0 3rem;
+`;
+
+const StyledP = styled.p`
+	font-size: 1.4rem;
+	font-weight: bold;
+	white-space: nowrap;
+`;
+
+export default Header;
