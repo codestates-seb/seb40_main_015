abf247fc6fb575d202715ea44c02499ecc6506f2
leeseongjun <thwn40@naver.com>
2022-12-01 17:41:57 +0900

    Refactor : RedisChat LocalDateTime 역직렬화 문제 해결

--- backend/src/main/java/com/dongnebook/domain/chat/domain/RedisChat.java
+++ backend/src/main/java/com/dongnebook/domain/chat/domain/RedisChat.java
@@ -5,6 +5,11 @@ import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,7 +25,8 @@ public class RedisChat {
 
 	@NotBlank
 	private String message;
-
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	private LocalDateTime createdAt;
 
 	public RedisChat(@NotNull Long roomId, @NotNull Long senderId, @NotBlank String message,
