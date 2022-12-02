2d4a7ae004abc5c0446b813321ae9f862676a859
fejigu <102123710+anjigu@users.noreply.github.com>
2022-12-02 16:48:57 +0900

    feat: pc version hero image produce

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -5,26 +5,34 @@ const MainPage = () => {
 	return (
 		<div>
 			<Body>
-				<div className="phone"></div>
+				<img src="" alt="메인 안내 이미지1" className="heroimage" />
 			</Body>
 		</div>
 	);
 };
 
 const Body = styled.div`
-	background-color: #016241;
-	width: 100vw;
-	height: 100vh;
-
-	.phone {
-		background-color: #ffffff;
-		width: 307px;
-		height: 664px;
-		border-radius: 20px;
-		align-items: center;
-		justify-content: center;
-		display: flex;
+	.heroimage {
+		width: 100%;
+		height: 375px;
+		background-color: red;
 	}
 `;
+
+// const Body = styled.div`
+// 	background-color: #016241;
+// 	width: 100vw;
+// 	height: 100vh;
+
+// 	.phone {
+// 		background-color: #ffffff;
+// 		width: 307px;
+// 		height: 664px;
+// 		border-radius: 20px;
+// 		align-items: center;
+// 		justify-content: center;
+// 		display: flex;
+// 	}
+// `;
 
 export default MainPage;
