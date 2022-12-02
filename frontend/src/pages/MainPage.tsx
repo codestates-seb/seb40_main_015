ddace406cca5fa5f96c21b11d6fb0c35a2a4f73e
fejigu <102123710+anjigu@users.noreply.github.com>
2022-12-02 18:42:47 +0900

    [FE] Feat : 메인 페이지 hero section 구성 및 라우터 수정 (#288)
    * fix: 내 정보 수정하기 및 모달창 CSS 수정
    
    * feat: 마이페이지 및 내정보수정하기페이지 css 수정
    
    * feat: 마이페이지 및 내정보수정하기 페이지 Css 수정 및 프로필 이미지 등록 기능 구현
    
    * feat: 사진 등록 작업중
    
    * feat: 마이페이지 - 예약목록, 프로필 이미지 등록, 현재 위치
    
    * feat : react-geocode
    
    * 내정보수정하기 페이지 저장 기능 구현
    
    * feat: window confirm 추가
    
    * fix: 사용하지 않는 import 목록 정리
    
    * 내동네설정 모달창 아니오 클릭시 뒤로가기 기능 구현
    
    * feat: 내정보수정하기 페이지 작업중
    
    * feat: geocoder
    
    * feat: 예약목록 interface 및 api
    
    * fix: 예약목록 불러오기 수정중
    
    * feat: 현재 위치 주거래 지역 설정 기능 구현 완료
    
    * feat: 내동네설정 예 버튼 기능 구현
    
    * feat: 내동네설정, 유저 이미지 등록
    
    * fix: geocoder env 파일 수정
    
    * fix: 등록한 도서수
    
    * fix: 사진 등록 png도 가능하게 수정
    
    * fix: 유저 이미지 수정중
    
    * 내정보수정하기 수정중
    
    * fix: api 폴더 구조 및 유저 이미지 업로드 수정
    
    * fix: 경로 수정
    
    * profileeditpage 주석 정리
    
    * feat: 마이페이지 및 내정보수정하기
    
    * feat: 상인페이지 이동 링크 작업중
    
    * feat: 상인페이지 이동 링크
    
    * feat: 마이페이지 책 상태 컴포넌트
    
    * feat: 닉네임 글자수 제한
    
    * feat: 컴포넌트 - 팀원배너
    
    * fix: 내정보수정하기 페이지 인풋창 반으로 줄어드는 것 수정
    
    * feat: 마이페이지 반응형으로 작업
    
    * 반응형
    
    * feat: 내정보수정하기페이지 반응형 작업중
    
    * feat: 반응형
    
    * feat: 탭 반응형으로 수정
    
    * feat: 로케이션 수정 및 등록한 도서 수 수정
    
    * fix: 의존성 배열 수정
    
    * fix: 상인 정보 연결 링크 수정
    
    * fix: 인풋창에 변경된 주소 들어가게 수정
    
    * fix: 상인 페이지로 이동 수정
    
    * feat: 중복 확인 작업중
    
    * fix: 추가 충돌 해결
    
    * feat: 상인정보페이지 반응형
    
    * fix: 상인페이지 문구 수정
    
    * fix: 상인페이지 유저 프로필 css 수정
    
    * fix: 예약목록 css 수정
    
    * feat: 예약 취소 버튼 css 추가
    
    * fix: 마이페이지 헤더 페딩 제거
    
    * feat: 중복확인 알람
    
    * feat: 반응형 네브바 없애기
    
    * feat: css 작업중
    
    * feat: 예약 취소 작업중
    
    * fix: 모달 가운데 정렬
    
    * 스크롤 작업중
    
    * fix:내정보수정하기 반응형
    
    * 마이페이지,상인페이지 책 제목 글자수 제한
    
    * feat: pc version hero image produce
    
    * feat: 메인 페이지 지정 및 라우터 수정
    
    * feat: mobile hero section

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import hero2 from '../assets/image/pc2.png';
+import hero3 from '../assets/image/pc3.png';
+import hero4 from '../assets/image/pc4.png';
+import hero5 from '../assets/image/pc5.png';
+import hero6 from '../assets/image/pc6.png';
+import hero7 from '../assets/image/pc7.png';
+import hero1 from '../assets/image/pc1.png';
 
 const MainPage = () => {
 	return (
 		<div>
 			<Body>
-				<img src="" alt="메인 안내 이미지1" className="heroimage" />
+				<img src={hero2} alt="동네북이란" className="heroimage" />
+				<img src={hero3} alt="지도 서비스" className="heroimage" />
+				<img src={hero4} alt="내 동네 설정" className="heroimage" />
+				<img src={hero5} alt="전체 조회 및 책 등록" className="heroimage" />
+				<img src={hero6} alt="대여 및 예약 캘린더" className="heroimage" />
+				<img src={hero7} alt="반응형 웹앱" className="heroimage" />
+				<img src={hero1} alt="메인 동네북" className="heroimage" />
 			</Body>
 		</div>
 	);
 };
 
 const Body = styled.div`
-	.heroimage {
+	transition-duration: 0.8s;
+	body {
+		margin: 0;
+		padding: 0;
+	}
+	img {
 		width: 100%;
-		height: 375px;
-		background-color: red;
+		height: 100%;
+		text-align: center;
 	}
 `;
+
+export default MainPage;
 
 // const Body = styled.div`
 // 	background-color: #016241;
@@ -34,5 +54,3 @@ const Body = styled.div`
 // 		display: flex;
 // 	}
 // `;
-
-export default MainPage;
