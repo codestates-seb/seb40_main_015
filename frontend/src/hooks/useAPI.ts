766577dbcc4de2f9acee780fcfe02d402fe4996d
Withlaw <98410418+Withlaw@users.noreply.github.com>
2022-12-02 21:41:13 +0900

    [FE] feat: 액세스토큰 갱신 로직 추가 (#294)
    * Fix: 라우팅 탭 부분  제거
    
    * Feat: 메인페이지 스타일 1
    
    * Feat: 메인페이지 스타일 2
    
    * design: 타이틀 로고 로직 추가
    
    * design: 메인페이지 스타일 완성1
    
    * Fix: 레이아웃깨짐 수정
    
    * feat: 도서상세페이지 html/css 구현
    
    * feat: 홈 화면에서 글 클릭시 책 상세조회 페이지로 넘어가는 기능 추가
    
    * feat: axios 기본설정
    
    * design: 책상세페이지 책제목, 상인이름및아바타 크기 조정
    
    * html 메니페스트 제거
    
    * 무한스크롤 컴포넌트 배경 회색처리
    
    * 찜 수정중
    
    * 찜 수정2
    
    * 예약취소 1
    
    * feat: 마이페이지 예약취소 구현
    
    * fix: 책 상세조희시 이전 도서 데이터가 깜빡이는 현상 수정
    
    * 책상세조회 페이지 깜빡임 현상 재수정
    
    * fix: 찜하기 refetch 함수 추가
    
    * 마이페이지 수정, pc 크기에서 회원정보 수정 표시
    
    * feat: 액세스토큰 만료 1분전 갱신 요청, 실패시 로그아웃
    
    * 토큰 갱신 재요청 없음으로 수정
    
    * .
    
    * design: 상인페이지 상태버튼 수정
    
    * 책상세조회페이지 책상태 오타 수정
    
    * design: 대여/예약하기 스타일 수정
    
    * 찜목록 상태버튼 사이즈 수정
    
    * 브라우저를 나갔다가 들어왔을때 토큰 만료 에러 1차 픽스
    
    Co-authored-by: Withlaw <Withlaw@github.io>

--- frontend/src/hooks/useAPI.ts
+++ frontend/src/hooks/useAPI.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import { BASE_URL } from '../constants/constants';
 import { useAppSelector } from '../redux/hooks';
+import { login } from '../redux/slice/userSlice';
 
 const useAPI = () => {
+	const dispatch = useDispatch();
 	const { accessToken } = useAppSelector(state => state.loginInfo);
 	const config = {
 		baseURL: BASE_URL,
@@ -11,6 +14,13 @@ const useAPI = () => {
 		// timeout: 10000,
 	};
 	const axiosWithAccessToken = axios.create(config);
+	axiosWithAccessToken.interceptors.response.use(
+		response => response,
+		err => {
+			if (err.response.data.message !== 'Token Expired') return;
+			dispatch(login({ accessToken: 'Bearer ', isLogin: true }));
+		},
+	);
 
 	return axiosWithAccessToken;
 };
