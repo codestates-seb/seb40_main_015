5a314ff3dade74b4f8b36a859947393bcfabbff7
jujang <jeongookjang@naver.com>
2022-12-06 23:54:24 +0900

    Infra: auto deploy 기본 설정 지정 - application 파일 기본설정 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,5 +28,4 @@ fi
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포 $DEPLOY_JAR" >> /home/ubuntu/action/backend/deploy.log
 #sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=rds 1> /home/ubuntu/action/backend/deploy.log 2>/home/ubuntu/action/backend/deploy_err.log
-#sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=rds > /home/ubuntu/action/backend/deploy.log 2>&1 &
-sudo nohup java -jar $DEPLOY_JAR > /home/ubuntu/action/backend/deploy.log 2>&1 &
+sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=rds > /home/ubuntu/action/backend/deploy.log 2>&1 &
