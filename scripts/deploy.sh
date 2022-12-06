a2e3e275113e0cda3dce726c51b6769b047180f2
jujang <jeongookjang@naver.com>
2022-12-06 15:54:54 +0900

    Infra: modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "> 현재 시간: $(date)" >> /home/ubuntu/action/backend/deploy.log
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/backend/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/action/backend/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
+DEPLOY_PATH=/home/ubuntu/action/backend/build/libs
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/backend/deploy.log
@@ -18,7 +18,7 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/backend/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
+  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/backend/deploy.log
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
