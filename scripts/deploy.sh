493e490627e272bcfb5b52a127265cbe70559099
jujang <jeongookjang@naver.com>
2022-12-06 16:02:08 +0900

    Infra: modify deploy file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "> 현재 시간: $(date)" >> /home/ubuntu/action/backend/deploy.log
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/backend/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/action/backend/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/backend/build/libs
+DEPLOY_PATH=/home/ubuntu/action/backend/build/libs/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/backend/deploy.log
