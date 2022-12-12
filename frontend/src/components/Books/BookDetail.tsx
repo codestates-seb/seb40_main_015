64780bc411697960d737566aee502673ffb6b541
jooyong-boo <qnwndyd159@naver.com>
2022-12-12 21:11:12 +0900

    feat: 리뷰열람 구현완료 및 기존 평점에 별점시스템 추가

--- frontend/src/components/Books/BookDetail.tsx
+++ frontend/src/components/Books/BookDetail.tsx
@@ -26,6 +26,7 @@ import { useBooksAPI } from '../../api/books';
 
 // types
 import { BookDetailProps } from './type';
+import Star from '../common/Star';
 
 const BookDetail = ({ book, merchant, refetchBookDetail }: BookDetailProps) => {
 	const { id, isLogin } = useAppSelector(state => state.loginInfo);
@@ -127,7 +128,7 @@ const BookDetail = ({ book, merchant, refetchBookDetail }: BookDetailProps) => {
 						<MerchantImg src={merchant?.avatarUrl} />
 						<MerchantName>
 							<span>{merchant?.name}</span>
-							<span>평점: {merchant?.grade} /5</span>
+							<span>평점: {<Star reviewGrade={merchant?.grade} />}</span>
 						</MerchantName>
 					</Link>
 					<Chat>
@@ -221,6 +222,8 @@ const MerchantName = styled.div`
 	display: flex;
 	flex-direction: column;
 	span:last-child {
+		display: flex;
+		align-items: center;
 		margin-left: 2px;
 		font-size: 12px;
 	}
