8dcde073a46ab3d1c12536d432578d78a9205304
leeseongjun <thwn40@naver.com>
2023-03-07 16:32:09 +0900

    [BE] Test : @SpringBootTest 제거

--- backend/src/test/java/com/dongnebook/DongnebookApplicationTests.java
+++ backend/src/test/java/com/dongnebook/DongnebookApplicationTests.java
@@ -1,9 +1,7 @@
 package com.dongnebook;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DongnebookApplicationTests {
 	@Test
 	void contextLoads() {
--- backend/src/test/java/com/dongnebook/domain/rental/application/RentalServiceTest.java
+++ backend/src/test/java/com/dongnebook/domain/rental/application/RentalServiceTest.java
@@ -12,7 +12,6 @@ import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.dongnebook.domain.book.domain.Book;
@@ -28,7 +27,6 @@ import com.dongnebook.domain.rental.ui.RentalController;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootTest
 @Slf4j
 class RentalServiceTest {
 
