60f4c4c1df1255893720ff00dfab3f0b3ccca3c5
jujang <jeongookjang@naver.com>
2023-03-28 20:27:08 +0900

    [BE] Test : Reservation Domain UnitTest (#419)

--- backend/src/test/java/com/dongnebook/config/TestConfig.java
+++ backend/src/test/java/com/dongnebook/config/TestConfig.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import com.dongnebook.domain.rental.repository.RentalQueryRepository;
+import com.dongnebook.domain.reservation.repository.ReservationQueryRepository;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.TaskExecutor;
@@ -47,6 +48,11 @@ public class TestConfig {
 	public RentalQueryRepository rentalQueryRepository() {
 		return new RentalQueryRepository(jpaQueryFactory());
 	}
+	@Bean
+	public ReservationQueryRepository reservationQueryRepository() {
+		return new ReservationQueryRepository(jpaQueryFactory());
+	}
+
 	@Bean(name = "taskExecutor")
 	public TaskExecutor clientInboundChannelExecutor() {
 		// ...
