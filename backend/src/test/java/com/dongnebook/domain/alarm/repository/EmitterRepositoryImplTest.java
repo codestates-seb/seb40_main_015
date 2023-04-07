2ffe68a3ee02d705196e29c658f8c859608fd4f5
jujang <jeongookjang@naver.com>
2023-04-07 23:37:51 +0900

    [BE] Test: EmitterRepositoryImpl modify & Unit Test (#435)

---
+++ backend/src/test/java/com/dongnebook/domain/alarm/repository/EmitterRepositoryImplTest.java
@@ -0,0 +1,268 @@
+package com.dongnebook.domain.alarm.repository;
+
+import com.dongnebook.domain.alarm.domain.Alarm;
+import com.dongnebook.domain.book.domain.Book;
+import com.dongnebook.domain.book.domain.BookProduct;
+import com.dongnebook.domain.book.domain.Money;
+import com.dongnebook.domain.member.domain.Member;
+import com.dongnebook.domain.model.Location;
+import com.dongnebook.global.enums.AlarmType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class EmitterRepositoryImplTest {
+    private EmitterRepository emitterRepository = new EmitterRepositoryImpl();
+    private Long DEFAULT_TIMEOUT = 60L * 1000L * 60L;
+
+    @Test
+    public void saveTest() throws Exception {
+        // given
+        Long memberId = 1L;
+        String emitterId =  memberId + "_" + System.currentTimeMillis();
+        SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
+
+        // when & then
+        Assertions.assertDoesNotThrow(() -> emitterRepository.save(emitterId, sseEmitter));
+    }
+
+    @Test
+    public void saveEventCacheTest() throws Exception {
+        // given
+        Long memberId = 1L;
+        String eventCacheId =  memberId + "_" + System.currentTimeMillis();
+
+        Member member = new Member(1L, "test", "tester", new Location(37.5340, 126.7064), "abcd", "aaa@aa.com");
+        BookProduct bookProduct1 = new BookProduct("수학의 정석1", "남궁성", "도우출판");
+        Book book = Book.builder()
+                .id(1L)
+                .bookProduct(bookProduct1)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm = new Alarm(member, book, AlarmType.MESSAGE, false);
+
+        // when & then
+        Assertions.assertDoesNotThrow(() -> emitterRepository.saveEventCache(eventCacheId, alarm));
+    }
+
+    @Test
+    public void findAllEmitterStartWithByMemberIdTest() throws Exception {
+        // given
+        Long memberId = 1L;
+        String emitterId1 = memberId + "_" + System.currentTimeMillis();
+        emitterRepository.save(emitterId1, new SseEmitter(DEFAULT_TIMEOUT));
+
+        Thread.sleep(100);
+        String emitterId2 = memberId + "_" + System.currentTimeMillis();
+        emitterRepository.save(emitterId2, new SseEmitter(DEFAULT_TIMEOUT));
+
+        Thread.sleep(100);
+        String emitterId3 = memberId + "_" + System.currentTimeMillis();
+        emitterRepository.save(emitterId3, new SseEmitter(DEFAULT_TIMEOUT));
+
+        // when
+        Map<String, SseEmitter> ActualResult = emitterRepository.findAllEmitterStartWithMemberId(memberId);
+
+        // then
+        Assertions.assertEquals(3, ActualResult.size());
+    }
+
+    @Test
+    public void findAllEventCacheStartWithByMemberIdTest() throws Exception {
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "test", "tester", new Location(37.5340, 126.7064), "abcd", "aaa@aa.com");
+
+        String eventCacheId1 =  memberId + "_" + System.currentTimeMillis();
+        BookProduct bookProduct1 = new BookProduct("수학의 정석1", "남궁성", "도우출판");
+        Book book1 = Book.builder()
+                .id(1L)
+                .bookProduct(bookProduct1)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm1 = new Alarm(member, book1, AlarmType.MESSAGE, false);
+        emitterRepository.saveEventCache(eventCacheId1, alarm1);
+
+        Thread.sleep(100);
+        String eventCacheId2 =  memberId + "_" + System.currentTimeMillis();
+        BookProduct bookProduct2 = new BookProduct("수학의 정석2", "남궁성", "도우출판");
+        Book book2 = Book.builder()
+                .id(2L)
+                .bookProduct(bookProduct2)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm2 = new Alarm(member, book2, AlarmType.MESSAGE, false);
+        emitterRepository.saveEventCache(eventCacheId2, alarm2);
+
+        Thread.sleep(100);
+        String eventCacheId3 =  memberId + "_" + System.currentTimeMillis();
+        BookProduct bookProduct3 = new BookProduct("수학의 정석3", "남궁성", "도우출판");
+        Book book3 = Book.builder()
+                .id(3L)
+                .bookProduct(bookProduct3)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm3 = new Alarm(member, book3, AlarmType.MESSAGE, false);
+        emitterRepository.saveEventCache(eventCacheId3, alarm3);
+
+        //when
+        Map<String, Object> ActualResult = emitterRepository.findAllEventCacheStartWithMemberId(memberId);
+
+        //then
+        Assertions.assertEquals(3, ActualResult.size());
+    }
+
+    @Test
+    public void deleteByIdTest() throws Exception {
+        //given
+        Long memberId = 1L;
+        String emitterId =  memberId + "_" + System.currentTimeMillis();
+        SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
+
+        //when
+        emitterRepository.save(emitterId, sseEmitter);
+        emitterRepository.deleteById(emitterId);
+
+        //then
+        Assertions.assertEquals(0, emitterRepository.findAllEmitterStartWithMemberId(memberId).size());
+    }
+
+    @Test
+    public void deleteAllEmitterStartWithIdTest1() throws Exception {
+        //given
+        Long memberId = 1L;
+        String emitterId1 = memberId + "_" + System.currentTimeMillis();
+        emitterRepository.save(emitterId1, new SseEmitter(DEFAULT_TIMEOUT));
+
+        Thread.sleep(100);
+        String emitterId2 = memberId + "_" + System.currentTimeMillis();
+        emitterRepository.save(emitterId2, new SseEmitter(DEFAULT_TIMEOUT));
+
+        //when
+        emitterRepository.deleteAllEmitterStartWithMemberId(memberId);
+
+        //then
+        Assertions.assertEquals(0, emitterRepository.findAllEmitterStartWithMemberId(memberId).size());
+    }
+
+    @Test
+    public void deleteAllEmitterStartWithIdTest2() throws Exception {
+        //given
+        Long memberId = 1L;
+        String emitterId1 = memberId + "_" + System.currentTimeMillis();
+        emitterRepository.save(emitterId1, new SseEmitter(DEFAULT_TIMEOUT));
+
+        Thread.sleep(100);
+        String emitterId2 = memberId + "_" + System.currentTimeMillis();
+        emitterRepository.save(emitterId2, new SseEmitter(DEFAULT_TIMEOUT));
+
+        //when
+        emitterRepository.deleteAllEmitterStartWithMemberId(null);
+
+        //then
+        Assertions.assertEquals(2, emitterRepository.findAllEmitterStartWithMemberId(memberId).size());
+    }
+
+    @Test
+    public void deleteAllEventCacheStartWithIdTest1() throws Exception {
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "test", "tester", new Location(37.5340, 126.7064), "abcd", "aaa@aa.com");
+
+        String eventCacheId1 =  memberId + "_" + System.currentTimeMillis();
+        BookProduct bookProduct1 = new BookProduct("수학의 정석1", "남궁성", "도우출판");
+        Book book1 = Book.builder()
+                .id(1L)
+                .bookProduct(bookProduct1)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm1 = new Alarm(member, book1, AlarmType.MESSAGE, false);
+        emitterRepository.saveEventCache(eventCacheId1, alarm1);
+
+        Thread.sleep(100);
+        String eventCacheId2 =  memberId + "_" + System.currentTimeMillis();
+        BookProduct bookProduct2 = new BookProduct("수학의 정석2", "남궁성", "도우출판");
+        Book book2 = Book.builder()
+                .id(2L)
+                .bookProduct(bookProduct2)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm2 = new Alarm(member, book2, AlarmType.MESSAGE, false);
+        emitterRepository.saveEventCache(eventCacheId2, alarm2);
+
+        //when
+        emitterRepository.deleteAllEventCacheStartWithMemberId(memberId);
+
+        //then
+        Assertions.assertEquals(0, emitterRepository.findAllEventCacheStartWithMemberId(memberId).size());
+    }
+
+    @Test
+    public void deleteAllEventCacheStartWithIdTest2() throws Exception {
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "test", "tester", new Location(37.5340, 126.7064), "abcd", "aaa@aa.com");
+
+        String eventCacheId1 =  memberId + "_" + System.currentTimeMillis();
+        BookProduct bookProduct1 = new BookProduct("수학의 정석1", "남궁성", "도우출판");
+        Book book1 = Book.builder()
+                .id(1L)
+                .bookProduct(bookProduct1)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm1 = new Alarm(member, book1, AlarmType.MESSAGE, false);
+        emitterRepository.saveEventCache(eventCacheId1, alarm1);
+
+        Thread.sleep(100);
+        String eventCacheId2 =  memberId + "_" + System.currentTimeMillis();
+        BookProduct bookProduct2 = new BookProduct("수학의 정석2", "남궁성", "도우출판");
+        Book book2 = Book.builder()
+                .id(2L)
+                .bookProduct(bookProduct2)
+                .imgUrl("aaa@abc.com")
+                .description("기본이 중요")
+                .rentalFee(Money.of(1000))
+                .location(new Location(37.5340, 126.7064))
+                .member(member)
+                .build();
+        Alarm alarm2 = new Alarm(member, book2, AlarmType.MESSAGE, false);
+        emitterRepository.saveEventCache(eventCacheId2, alarm2);
+
+        //when
+        emitterRepository.deleteAllEventCacheStartWithMemberId(null);
+
+        //then
+        Assertions.assertEquals(2, emitterRepository.findAllEventCacheStartWithMemberId(memberId).size());
+    }
+}
