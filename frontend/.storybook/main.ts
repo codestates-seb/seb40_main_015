b0bde76dd6f00f4a842d7e567f75ba7f5f58d12e
jooyong-boo <qnwndyd159@naver.com>
2023-04-18 00:28:14 +0900

    feat: storybook addon 추가

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -7,6 +7,8 @@ const config: StorybookConfig = {
 		'@storybook/preset-create-react-app',
 		'@storybook/addon-interactions',
 		'@storybook/addon-styling',
+		'@storybook/addon-actions',
+		'@storybook/addon-controls',
 	],
 	framework: {
 		name: '@storybook/react-webpack5',
