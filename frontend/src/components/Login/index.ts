6e06d4183d885af3272382c3b7330e8034ed0dda
jooyong-boo <qnwndyd159@naver.com>
2023-04-18 00:29:43 +0900

    remove: oauth 미사용으로 관련 코드 및 컴포넌트 제거

--- frontend/src/components/Login/index.ts
+++ frontend/src/components/Login/index.ts
@@ -1,5 +1,4 @@
 export { default as LinkToSignUp } from 'components/Login/LinkToSignUp';
 export { default as LoginForm } from 'components/Login/LoginForm';
 export { default as LoginLogo } from 'components/Login/Logo';
-export { default as LoginOauth } from 'components/Login/Oauth';
 export { default as ThirdPartyIcon } from 'components/Login/ThirdPartyIcon';
